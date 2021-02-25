e76ccfea264ada6f0827fe23ab72766433dca7f6
John Sigmon <johnsigmon@gmail.com>
2021-02-24 20:42:12 -0700

    add svelte

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
+path_to_static_dir = "../client/public"
+
 @app.route("/")
-def home():
-    return "<button> Hello world </button>"
+def base():
+    return send_from_directory(path_to_static_dir, "index.html")
+
+@app.route("/<path:path>")
+def home(path):
+    return send_from_directory(path_to_static_dir, path)
 
 if __name__ == "__main__":
     app.run()
